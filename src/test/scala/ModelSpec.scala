600a41f46b6ec743f11083641d7827f8345dae48
vitaly.krokhalev <vitaly.krokhalev@elementpay.io>
2020-06-07 00:09:38 +0700

    Test renamed.

--- src/test/scala/ModelSpec.scala
+++ src/test/scala/ModelSpec.scala
@@ -1,7 +1,7 @@
 import org.scalatest.flatspec.AsyncFlatSpec
 import ru.sibsutis.application.model.AccessControlModel
 
-class ModelSuite extends AsyncFlatSpec {
+class ModelSpec extends AsyncFlatSpec {
   behavior of "createModel"
   it should "eventually get a values of experiments" in {
     AccessControlModel.createModel map { values => assert(values.nonEmpty) }
